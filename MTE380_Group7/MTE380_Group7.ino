5c2dbc3ad860e665aea78d5c9d8e6a240f916dcc
clfowler11 <catherine.fowler11@gmail.com>
2019-03-21 22:20:30 -0400

    comment clarification
    It will call this even as it approaches a tile intended. But it won't actually trigger anything because the tile will already be identified as a goal.

--- MTE380_Group7/MTE380_Group7.ino
+++ MTE380_Group7/MTE380_Group7.ino
@@ -521,7 +521,7 @@ void FindingFood() {
 void Travelling() { // Navigation without searching
   // If we are gonna hit something, don't.
   if (!centering && !turning && front_dist < 300 && front_dist != -1) {
-    ObjectOnTile(); // this should set the thing ahead of us as a possibility
+    ObjectOnTile(); // this should set the thing ahead of us as a possibility if needed
   }
   
   // If we began centering, finish centering
